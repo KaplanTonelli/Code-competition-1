edfdd20c86ce15d7e36c9c8e27a062a5f923ecef
kaplantonelli <kaplantonelli@gmail.com>
2026-09-15 10:52:42 -0600

    Countdown

---
+++ countdown.py
@@ -0,0 +1,27 @@
+"""
+Code for the Countdown code problem
+Kaplan Tonelli - September 2026
+"""
+
+def main() -> None:
+
+  # input
+  n:int=int(input(""))
+  m:float=n
+  # processing
+  for i in range(n):
+    if m==m:
+      print(m)
+      m-=1
+    else:
+      m-=1
+      print(m)
+
+
+  
+  # output
+
+
+if __name__ == "__main__":
+  main()
+    
