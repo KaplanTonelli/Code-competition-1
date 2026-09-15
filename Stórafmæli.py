7c885f481c9c2ed2a87f18e2a8f70a4d09ab62ec
kaplantonelli <kaplantonelli@gmail.com>
2026-09-15 11:03:20 -0600

    storafmaeli

---
+++ Stórafmæli.py
@@ -0,0 +1,28 @@
+"""
+Code for the Stórafmæli code problem
+Kaplan Tonelli - September 2026
+"""
+
+def main() -> None:
+
+  # input
+  n:int=int(input(""))
+  # processing
+  if n/10 == 1:
+    print("Jebb")
+  elif n/10 == 2:
+    print("Jebb")
+  elif n/10 == 3:
+    print("Jebb")
+  elif n/10 == 4:
+    print("Jebb")
+  elif n/10 == 5:
+    print("Jebb")
+  else:
+    print("Neibb")
+  # output
+
+
+if __name__ == "__main__":
+  main()
+    
