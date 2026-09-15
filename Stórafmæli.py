2e34ef916920f770378edf6c0e1a6b053c9e27f2
kaplantonelli <kaplantonelli@gmail.com>
2026-09-15 11:26:20 -0600

    sotrafmaeli

--- Stórafmæli.py
+++ Stórafmæli.py
@@ -8,15 +8,7 @@ def main() -> None:
   # input
   n:int=int(input(""))
   # processing
-  if n/10 == 1:
-    print("Jebb")
-  elif n/10 == 2:
-    print("Jebb")
-  elif n/10 == 3:
-    print("Jebb")
-  elif n/10 == 4:
-    print("Jebb")
-  elif n/10 == 5:
+  if n%10 == 0:
     print("Jebb")
   else:
     print("Neibb")
