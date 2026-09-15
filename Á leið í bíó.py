14034c32a61e5ca1d7a0e4c605a551d8eba1bb25
kaplantonelli <kaplantonelli@gmail.com>
2026-09-15 11:31:30 -0600

    a Leio

---
+++ Á leið í bíó.py
@@ -0,0 +1,19 @@
+"""
+Code for the Á leið í bíó code problem
+Kaplan Tonelli - September 2026
+"""
+
+def main() -> None:
+
+  # input
+  a:int=int(input(""))
+  b:int=int(input(""))
+  c:int=int(input(""))
+  # processing
+  print(c-b-a)
+  # output
+
+
+if __name__ == "__main__":
+  main()
+    
